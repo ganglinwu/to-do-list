59533556473b6ce156e6af2055bad5ba98055f5a
ganglinwu <ganglin.wu@gmail.com>
2023-12-04 17:34:41 +0800

    fix: gyh is no longer an IIFE
    problem with IIFE:
    - other functions/block scopes cannot access projects since it is protected under
      closure

--- src/index.js
+++ src/index.js
@@ -18,38 +18,40 @@ import Project from './projects.js';
 // i.e. gyh >> Project >> ToDo
 // personally i think a different name would be less confusing thus gyh
 
-export const projectArray = (function () {
-    const projects = [];
+export const gyh = {
+    projects: {
+        '< 2 mins': new Project('< 2 mins', 'tasks that take less than 2 minutes to complete'),
+        '< 10 mins': new Project('< 10 mins', 'tasks that take less than 10 minutes to complete'),
+        '< 30 mins': new Project('< 30 mins', 'tasks that take less than 30 minutes to complete'),
+        '< 60 mins': new Project('< 60 mins', 'tasks that take less than 60 minutes to complete'),
+        '> 60 mins': new Project('> 60 mins', 'tasks that take more than 60 minutes to complete'),
+    }, 
 
-    projects.push(new Project('< 2 mins', 'tasks that take less than 2 minutes to complete')) 
-    projects.push(new Project('< 5 mins', 'tasks that take less than 5 minutes to complete'))
-    projects.push(new Project('< 10 mins', 'tasks that take less than 10 minutes to complete')) 
-    projects.push(new Project('< 30 mins', 'tasks that take less than 30 minutes to complete')) 
-    projects.push(new Project('< 60 mins', 'tasks that take less than 60 minutes to complete')) 
-    projects.push(new Project('> 60 mins', 'tasks that take more than 60 minutes to complete')) 
 
-    const add = (Project) => projects.push(Project)
-    const remove = (Project) => {
-        if (projects.some(Project)) {
-            const index = projects.indexOf(Project);
-            projects.splice(index, 1);
+    add: function(Proj){
+        this.projects[Proj.title] = Proj
+    },
+
+   remove: function(Proj){
+        if (this.projects[Proj.title]) {
+            delete this.projects[Proj.title];    
+        } else {
+            alert(`${Proj.title} does not exist!`);
         }
-    }
-
-    return { projects, add, remove }
-   })();
+    },
+   };
 
 document.body.appendChild(loadHome());
 
 // helper function to refresh list of projects in sidebar
 export function loadSidebarProj() {
     const projListContainer = createEle('div');
-    projectArray.projects.forEach((Proj) => {
+    Object.keys(gyh.projects).forEach((key) => {
         const projDiv = createEle('div', 'class', 'projDiv');
-        if (Proj.title.length < 18) {
-            projDiv.innerText = Proj.title;
+        if (gyh.projects[key].title.length < 18) {
+            projDiv.innerText = gyh.projects[key].title;
         } else {
-            const shortenedProjTitle = Proj.title.slice(0,18);
+            const shortenedProjTitle = gyh.projects[key].title.slice(0,18);
             projDiv.innerText = shortenedProjTitle;
         }
         projDiv.addEventListener('click', loadTask)
