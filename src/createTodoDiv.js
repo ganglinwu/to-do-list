a39c971a5c58c98b08840f3f7bae3b8176544bd5
ganglinwu <ganglin.wu@gmail.com>
2023-12-27 16:59:12 +0800

    feat(todolist): ability to edit todo
    - added minimize button to toggle out of detailed view
    - user may click save button or use Enter key to save edits

--- src/createTodoDiv.js
+++ src/createTodoDiv.js
@@ -1,6 +1,7 @@
 import createEle from './createEle.js';
 import { revealDetails, hideDetails } from './revealdetails.js';
 import editIconSrc from './img/icons8-edit-26.png'
+import { gyh } from './index.js';
 
 export function createTodoDivShort(todo) {
     const todoDiv = createEle('div', 'class', 'todoDiv');
@@ -33,6 +34,16 @@ export function createTodoDivShort(todo) {
 export function createTodoDivDetailed(todo){
     const todoDiv = createEle('div', 'class', 'todoDivDetailed');
     todoDiv.classList.add('hidden');
+    
+    // initialize minimize button
+    const minimizeBtn = createEle('button', 'type', 'button');
+    minimizeBtn.classList.add('minimizeBtn');
+    const minimizeBtnText = createEle('p', 'class', 'minimizeBtnText');
+    minimizeBtnText.innerText = '-';
+
+    minimizeBtn.appendChild(minimizeBtnText);
+    todoDiv.appendChild(minimizeBtn);
+
     // render div for each property of todo
     const todoDivDetailedName = createEle('div', 'class', 'todoDivDetailedName');
     const todoDivDetailedDescription = createEle('div', 'class', 'todoDivDetailedDescription');
@@ -61,10 +72,17 @@ export function createTodoDivDetailed(todo){
         editIcon.style.width = '12px';
         editIcon.classList.add('editIcon');
 
+        // initialze save edit button
+        const saveEdit = createEle('button', 'type', 'button');
+        saveEdit.classList.add('saveEdit');
+        saveEdit.classList.add('hidden');
+        saveEdit.innerText = 'Save'
+
         // initialize divs
         const label = createEle('div', 'class', 'todoLabel');
         label.innerText = labelName;
         const content = createEle('div', 'class', 'content');
+        content.setAttribute('contenteditable', 'false');
         // check if Date object, we just want to concatenated date string
         if (todo[camelCaseName] instanceof Date) {
             content.innerText = todo[camelCaseName].toLocaleDateString();
@@ -74,7 +92,61 @@ export function createTodoDivDetailed(todo){
         element.appendChild(label);
         element.appendChild(content);
         element.appendChild(editIcon);
+        element.appendChild(saveEdit);
         todoDiv.appendChild(element);
+
+        //eventlistener for edit icon
+        editIcon.addEventListener('click', (e)=> {
+            editIcon.classList.toggle('hidden');
+            saveEdit.classList.toggle('hidden');
+
+            //move label upwards slightly
+            e.target.parentElement.firstChild.classList.toggle('up8px');
+
+            e.target.previousSibling.setAttribute('contenteditable', 'true');
+            e.target.previousSibling.style.border = '3px var(--main-blue) solid';
+            e.target.previousSibling.focus();
+
+            //eventlistener for enter and esc keyup
+            e.target.previousSibling.addEventListener('keydown', (keyEvt)=> {
+                if (keyEvt.key === 'Enter') {
+                    e.target.nextSibling.click()
+                }
+            })
+        })
+
+        //eventlistener for save edit button
+        saveEdit.addEventListener('click', (e)=> {
+            e.target.previousSibling.previousSibling.setAttribute('contenteditable', 'false');
+            e.target.previousSibling.previousSibling.style.border = 'none';
+            
+            // move label back down
+            e.target.parentElement.firstChild.classList.toggle('up8px');
+
+            // save changes to gyh.[projects]
+            const projTitle = e.target.parentElement.parentElement.parentElement.previousSibling.innerText;
+            const todoObjKey = e.target.parentElement.firstChild.innerText;
+            const todoObjKeyCamelCase = todoObjKey[0].toLowerCase() + todoObjKey.slice(1);
+            const indexNum = countIndexNum(e);
+
+            gyh.projects[projTitle].todoArray[indexNum][todoObjKeyCamelCase] = e.target.previousSibling.previousSibling.innerText;
+
+            editIcon.classList.toggle('hidden');
+            saveEdit.classList.toggle('hidden');
+
+            // refreshes todoDivShort
+            e.target.parentElement.parentElement.previousSibling.remove();
+            const newTodoDivShort = createTodoDivShort(gyh.projects[projTitle].todoArray[indexNum]);
+            newTodoDivShort.classList.add('hidden');
+            e.target.parentElement.parentElement.parentElement.insertBefore(newTodoDivShort, e.target.parentElement.parentElement);
+
+            // refreshes todoDivDetailed
+            const newTodoDivDetailed = createTodoDivDetailed(gyh.projects[projTitle].todoArray[indexNum]);
+            newTodoDivDetailed.classList.remove('hidden');
+            e.target.parentElement.parentElement.parentElement.insertBefore(newTodoDivDetailed, e.target.parentElement.parentElement.nextSibling);
+            e.target.parentElement.parentElement.remove();
+            console.log(gyh);
+        })
     })
 
     // add class according to priority
@@ -85,7 +157,7 @@ export function createTodoDivDetailed(todo){
    } else if (todo.priority === 'Low'){
         todoDiv.classList.add('lowPriority');
     }
-    todoDiv.addEventListener('click', hideDetails);
+    minimizeBtn.addEventListener('click', hideDetails);
     return todoDiv
 }
 
@@ -105,6 +177,21 @@ function removeUpperCamelCase(word) {
             result += word[i];
         }
     }
-    result += ':';
     return result
+}
+
+// helper function to determine the index number of the todo under a project based on click event of save edit button
+function countIndexNum(clickEvent) {
+    let todoDiv = clickEvent.target.parentElement.parentElement;
+    let count = 0;
+    // once we reach the first sibling, previousSibling will return null
+    while (todoDiv.previousSibling !== null) { 
+        todoDiv = todoDiv.previousSibling;
+        count+=1;
+    }
+    // we have twice the number of todoDivs compared to number of todos
+    // (todoDiv and todoDivDetailed)
+    // also indexing starts from zero, thus we use floor instead of ceiling function
+    // in fact count will almost always be odd, since the save edit button can only be found on odd todoDivs
+    return Math.floor(count/2);  
 }
