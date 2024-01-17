870d08d8ffe379a4844d85311f0299d2edd78756
ganglinwu <ganglin.wu@gmail.com>
2024-01-17 14:46:43 +0800

    feat(localStorage): enable persistent data store
    - added helper function to check availability of localStorage
    - refactor adding sample todos as helper function
    - if localStorage available app  will use it
    
    TODO: another helper function to update localStorage whenever changes
    are made to gyh object

--- src/index.js
+++ src/index.js
@@ -43,39 +43,23 @@ export const gyh = {
     },
 };
 
-/* ---------- TEST code to manually add todos ----------  */
-
-const dateToday = new Date();
-const currentDay = dateToday.getDate();
-const currentMonth = dateToday.getMonth();
-const currentYear = dateToday.getFullYear();
-
-//                                      constructor(name, description, dueDate, duration, completed, priority)
-gyh.projects['sample project'].todoArray.push(
-    new Todo(
-        'sample project',
-        'print pdf',
-        "for mum's visa application",
-        new Date(currentYear, currentMonth + 1, currentDay),
-        2,
-        false,
-        'High'
-    )
-);
-gyh.projects['sample project'].todoArray.push(
-    new Todo(
-        'sample project',
-        'water plants',
-        'green bean plant',
-        new Date(currentYear, currentMonth, currentDay),
-        1,
-        false,
-        'Low'
-    )
-);
-
-/* ---------- END OF TEST code to manually add todos ----------  */
-
+// check if localStorage is available
+if (storageAvailable('localStorage')) {
+    // check if gyh is already written to localStorage
+    if (localStorage.getItem(gyh) !== null) {
+        gyh.projects = JSON.parse(localStorage.getItem('gyh.projects'));
+    } else {
+        // if gyh is not in locaStorage then this is likely the first visit, let's write gyh into localStorage
+        // first populate with sample todos, then write into localStorage.
+        gyh.projects['sample project'].todoArray.push(
+            generateSampleTodoArray()
+        );
+        localStorage.setItem('gyh', JSON.stringify(gyh));
+    }
+} else {
+    // if no localStorage we just populate with sample todos
+    gyh.projects['sample project'].todoArray.push(generateSampleTodoArray());
+}
 // load home page
 document.body.appendChild(loadHome());
 
@@ -161,4 +145,66 @@ export function isProjRendered(projTitle) {
     } else return false;
 }
 
+// helper function to check available
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+// function takes argument of "localStorage" or "sessionStorage"
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            // everything except Firefox
+            (e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
+
+function generateSampleTodoArray() {
+    const dateToday = new Date();
+    const currentDay = dateToday.getDate();
+    const currentMonth = dateToday.getMonth();
+    const currentYear = dateToday.getFullYear();
+
+    const sampleTodoArray = [];
+    //                                      constructor(name, description, dueDate, duration, completed, priority)
+    sampleTodoArray.push(
+        new Todo(
+            'sample project',
+            'print pdf',
+            "for mum's visa application",
+            new Date(currentYear, currentMonth + 1, currentDay),
+            2,
+            false,
+            'High'
+        )
+    );
+    sampleTodoArray.push(
+        new Todo(
+            'sample project',
+            'water plants',
+            'green bean plant',
+            new Date(currentYear, currentMonth, currentDay),
+            1,
+            false,
+            'Low'
+        )
+    );
+    return sampleTodoArray;
+}
 /* -------------------- END OF helper functions --------------------  */
